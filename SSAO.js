568f1e9f4b9adfd67a1a1e1e99e40ca9d5a4d563
Marcin Ignac <marcin.ignac@gmail.com>
2016-01-27 15:07:58 +0000

    Removed old SSAO code

--- SSAO.js
+++ SSAO.js
@@ -35,21 +35,3 @@ FXStage.prototype.ssao = function (options) {
 };
 
 module.exports = FXStage;
-
-FXStage.prototype.ssaoOld = function (options) {
-  options = options || {};
-  var outputSize = this.getOutputSize(options.width, options.height);
-  var rt = this.getRenderTarget(outputSize.width, outputSize.height, options.depth, options.bpp);
-  rt.bind();
-  var depthMap = this.getSourceTexture(options.depthMap);
-  depthMap.bind(0);
-  var program = this.getShader(SSAOGLSL);
-  program.use();
-  program.uniforms.textureSize(Vec2.create(outputSize.width, outputSize.height));
-  program.uniforms.depthMap(0);
-  program.uniforms.near(options.camera.getNear());
-  program.uniforms.far(options.camera.getFar());
-  this.drawFullScreenQuad(outputSize.width, outputSize.height, null, program);
-  rt.unbind();
-  return this.asFXStage(rt, 'ssao');
-};
